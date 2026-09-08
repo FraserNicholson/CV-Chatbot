29224e10bf08ad867a3523690eaff58751752a53
Fraser Nicholson <fraser.nicholson@outlook.com>
2026-09-08 15:23:47 +0100

    Add web ui

---
+++ frontend/webui/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    const submitForm = async (event) => {
+        const formData = new FormData(event.target);
+
+        // [Use formData]
+        console.log([...formData]);
+
+        const inputElement = document.getElementById("input");
+        inputElement.value = "";
+    };
+</script>
+
+<h1>Welcome to Fraser Nicholson's CV Chatbot</h1>
+<form onsubmit={submitForm}>
+    <textarea
+        id="input"
+        name="input"
+        placeholder="Ask me a question about Fraser Nicholson"
+    >
+    </textarea>
+    <input type="submit" value="Submit" />
+</form>
